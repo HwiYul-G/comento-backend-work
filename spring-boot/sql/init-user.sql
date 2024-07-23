afc37e2094a5a8004deb7f134a040298079213c7
kim <hwiyul_g@daum.net>
2024-07-23 16:00:42 +0900

    table 생성 및 requestInfo와 user 초기화 sql문 추가

---
+++ spring-boot/sql/init-user.sql
@@ -0,0 +1,36 @@
+INSERT INTO statistic9.user(userID, HR_ORGAN, USERNAME)
+    VALUES(1, 'AAA', '신짱구'),
+    (2, 'AAA', '신형만'),
+    (3, 'AAA', '봉미선'),
+    (4, 'AAA', '신짱아'),
+    (5, 'AAA', '흰둥이'),
+    (6, 'AAA', '김철수'),
+    (7, 'AAA', '한유리'),
+    (8, 'AAA', '이훈이'),
+    (9, 'AAA', '맹구'),
+    (10, 'AAA', '한수지');
+INSERT INTO statistic9.user(userID, HR_ORGAN, USERNAME)
+    VALUES(11, 'BBB', '루피'),
+    (12, 'BBB', '크롱'),
+    (13, 'BBB', '포비'),
+    (14, 'BBB', '패티'),
+    (15, 'BBB', '뽀로로'),
+    (16, 'BBB', '크롱'),
+    (17, 'BBB', '페티'),
+    (18, 'BBB', '에디');
+INSERT INTO statistic9.user(userID, HR_ORGAN, USERNAME)
+    VALUES(19, 'CCC', '도라에몽'),
+    (20, 'CCC', '노진구'),
+    (21, 'CCC', '신이슬'),
+    (22, 'CCC', '만퉁퉁'),
+    (23, 'CCC', '왕비실'),
+    (24, 'CCC', '도라미');
+INSERT INTO statistic9.user(userID, HR_ORGAN, USERNAME)
+    VALUES(25, 'DDD', '코난'),
+    (26, 'DDD', '유미란'),
+    (27, 'DDD', '홍장미'),
+    (28, 'DDD', '유명한'),
+    (29, 'DDD', '세모'),
+    (30, 'DDD', '아름이'),
+    (31, 'DDD', '뭉치'),
+    (32, 'DDD', '유미란');
