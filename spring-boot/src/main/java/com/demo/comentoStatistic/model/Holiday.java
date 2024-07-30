4ef38feff602806ef262b8b727b3636a12a854fc
kim <hwiyul_g@daum.net>
2024-07-31 00:21:13 +0900

    Holiday model 추가

---
+++ spring-boot/src/main/java/com/demo/comentoStatistic/model/Holiday.java
@@ -0,0 +1,14 @@
+package com.demo.comentoStatistic.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Holiday {
+    private String localDate;
+
+    private String description;
+
+}
