612ab10d855f6e9dac5a383436810dc892a9b9eb
kim <hwiyul_g@daum.net>
2024-07-31 18:34:10 +0900

    holiday list를 한 번에 집어 넣는 sql문 추가(mybatis의 동적 sql)

--- spring-boot/src/main/java/com/demo/comentoStatistic/dao/HolidayMapper.java
+++ spring-boot/src/main/java/com/demo/comentoStatistic/dao/HolidayMapper.java
@@ -3,9 +3,12 @@ package com.demo.comentoStatistic.dao;
 import com.demo.comentoStatistic.model.Holiday;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface HolidayMapper {
 
     void insertHoliday(Holiday holiday);
 
+    void insertHolidays(List<Holiday> holidays);
 }
