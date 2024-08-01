a203e4dbd75216cbc7b7f980efa4e7fa0adae4f6
kim <hwiyul_g@daum.net>
2024-08-01 22:49:44 +0900

    daoAspect test code 추가

---
+++ spring-boot/src/test/java/com/demo/comentoStatistic/aop/DaoAspectTest.java
@@ -0,0 +1,34 @@
+package com.demo.comentoStatistic.aop;
+
+import com.demo.comentoStatistic.exception.DaoAccessException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+// Mockito를 가능하게 하는 어노테이션 추가
+// 자동적으로 mock들을 초기화하고 mock들을 우리의@InjectMocks 어노테이션된 테스팅 유닛에 삽입한다.
+@ExtendWith(MockitoExtension.class)
+class DaoAspectTest {
+    @Mock
+    private ProceedingJoinPoint proceedingJoinPoint;
+
+    @InjectMocks
+    private DaoAspect daoAspect;
+
+    @DisplayName("RuntimeException 발생시 DaoAccessException이 제대로 던져 지는지 확인")
+    @Test
+    void whenExecuteJoinPoint_thenDaoAccessExceptionIsThrown() throws Throwable {
+        when(proceedingJoinPoint.proceed()).thenThrow(new RuntimeException());
+
+        assertThrows(DaoAccessException.class, ()-> daoAspect.daoBefore(proceedingJoinPoint));
+        verify(proceedingJoinPoint, times(1)).proceed();
+    }
+
+}
