60dc028fa16f0d940f0bde0d5219695a770bd4fa
kim <hwiyul_g@daum.net>
2024-08-02 11:29:09 +0900

    logging시 구체 오류 볼 수 있도록 StackTrace 추가

--- spring-boot/src/main/java/com/demo/comentoStatistic/exception/ExceptionHandlerAdvice.java
+++ spring-boot/src/main/java/com/demo/comentoStatistic/exception/ExceptionHandlerAdvice.java
@@ -1,5 +1,6 @@
 package com.demo.comentoStatistic.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -10,11 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
+        e.getStackTrace();
+
         Map<String, String> errors = new HashMap<>();
         e.getBindingResult().getAllErrors().forEach((err) -> {
             String fieldName = ((FieldError) err).getField();
@@ -28,6 +32,8 @@ public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(DaoAccessException.class)
     public ResponseEntity<Object> handleDaoAccessException(DaoAccessException e) {
+        e.getStackTrace();
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(e.getMessage());
@@ -35,6 +41,8 @@ public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleOtherException(Exception e) {
+        e.getStackTrace();
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(e.getMessage());
